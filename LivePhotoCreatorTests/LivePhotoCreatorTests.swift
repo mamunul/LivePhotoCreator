2df8f59e4c5d1579fee4c8acc2aca7c78a725f2e
Md Mamunul Mazid <mamunul.mazid@gmail.com>
2021-03-23 17:50:28 +0600

    Initial Commit

---
+++ LivePhotoCreatorTests/LivePhotoCreatorTests.swift
@@ -0,0 +1,33 @@
+//
+//  LivePhotoCreatorTests.swift
+//  LivePhotoCreatorTests
+//
+//  Created by New User on 23/3/21.
+//
+
+import XCTest
+@testable import LivePhotoCreator
+
+class LivePhotoCreatorTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
